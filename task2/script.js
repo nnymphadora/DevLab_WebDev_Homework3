3fb4915f1b6c6fcc94ccdd0442218b57e5edbf28
nnymhadora <janagracanin@gmail.com>
2023-02-05 20:48:05 +0100

    Finished all the validation functions(palindrome checker and alpha input checker).  Also enabled input element removal on click of appropriate element.

--- task2/script.js
+++ task2/script.js
@@ -4,6 +4,7 @@ const addCharsBtn = document.querySelector("#add-chars-btn");
 const inputContainer = document.querySelector("#input-container");
 const boxContainer = document.querySelector("#box-container");
 const boxDiv = document.querySelector("#box-div");
+const statusDiv = document.querySelector("#status-div");
 
 confirmChartBtn.addEventListener("click", charactersConfirmed);
 addCharsBtn.addEventListener("click", addCharBox);
@@ -19,14 +20,23 @@ function createCharBoxes(repeatTimes) {
   if (repeatTimes === 0) {
     return;
   } else {
+    let box = document.createElement("div");
+    boxDiv.appendChild(box);
     let charBox = document.createElement("input");
     charBox.type = "text";
     charBox.maxLength = "1";
-    charBox.classList.add("char-box");
-    boxDiv.appendChild(charBox);
-    boxContainer.addEventListener("click", setTarget);
+    charBox.classList.add("char-box", "hover-remove-button");
+    box.appendChild(charBox);
+    charBox.addEventListener("input", checkInput);
 
-    charBox.focus();
+    let removeCharBoxBtn = document.createElement("span");
+    removeCharBoxBtn.classList.add("remove-chars-btn");
+    removeCharBoxBtn.textContent = " x ";
+
+    removeCharBoxBtn.addEventListener("click", removeCharBox);
+
+    box.appendChild(removeCharBoxBtn);
+
     createCharBoxes(repeatTimes - 1);
   }
 }
@@ -35,11 +45,33 @@ function addCharBox() {
   createCharBoxes(1);
 }
 
-function setTarget() {
+function removeCharBox() {
+  this.parentNode.remove();
+}
+
+function checkInput() {
   let charBoxes = document.querySelectorAll(".char-box");
+  inputString = "";
   charBoxes.forEach((box) => {
-    if (box.value === "") {
-      charBoxes[0].focus();
-    }
+    inputString += box.value;
   });
+  if (isAlpha(inputString)) {
+    if (isPalindrome(inputString)) {
+      statusDiv.textContent = `Wow! '${inputString}' is a palindrome!`;
+    } else {
+      statusDiv.textContent = `It looks like '${inputString}' is not a palindrome!`;
+    }
+  } else {
+    statusDiv.textContent =
+      "Oh no! Invalid input. Only letters and spaces are accepted.";
+  }
+}
+
+function isAlpha(input) {
+  return /^[a-zA-Z\s]+$/.test(input);
+}
+
+function isPalindrome(input) {
+  let inputString = input.toLowerCase().replace(/\s/g, "");
+  return inputString === inputString.split("").reverse().join("");
 }
